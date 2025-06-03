7126ed8080ddddd1dd4218ecba7c0c1bf8edf95e
Chelsea Cruz <125297600+chelsea-here@users.noreply.github.com>
2025-06-03 12:26:13 -0500

    modified folder location of node modules to fix errors

--- server/db.js
+++ server/db.js
@@ -1,1 +1,49 @@
 //data layer
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/the_acme_reservation_planner");
+const {v4} = require('uuid');
+const uuidv4 = v4;
+
+/* const createCustomer = async (customer) => {
+  const SQL = `
+  DROP IF EXISTS ;
+  CREATE TABLE table (
+  )
+  `
+
+} */
+
+const seed = async () => {
+  const SQL = `
+    DROP TABLE IF EXISTS reservations;
+    DROP TABLE IF EXISTS restaurants;
+    DROP TABLE IF EXISTS customers;
+
+    CREATE TABLE customers(
+      id UUID PRIMARY KEY,
+      name VARCHAR(100)
+      );
+
+    CREATE TABLE restaurants(
+      id UUID PRIMARY KEY,
+      name VARCHAR(100)
+      );
+
+    CREATE TABLE reservations(
+      id UUID PRIMARY KEY,
+      date TIMESTAMP DEFAULT now(),
+      party_count INTEGER NOT NULL,
+      restaurant_id UUID REFERENCES restaurants(id) NOT NULL,
+      customer_id UUID REFERENCES customers(id) NOT NULL
+    );
+    `
+  await client.query(SQL);
+  console.log('created tables');
+
+
+}
+
+module.exports = {
+  client, 
+  seed
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
 //express app and setup funcitons
+const express = require("express");
+const app = express();
+const { client, seed } = require('./db');
+app.use(express.json());
+
+
+
+const init = async () => {
+  try {
+    console.log("connecting to database")
+    await client.connect();
+    console.log("connected to database")
+    await seed();
+    /* const SQL = ``
+    await client.query(SQL)
+    console.log("seeded db"); 
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`)
+    }) */
+    
+  } catch (error) {
+    next(error)
+  }
+} 
+
+init();
